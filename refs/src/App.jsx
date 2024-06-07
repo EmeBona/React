34a3d486b39afb8ee50baf59beab814d63a4bc45
María Bona <mariabonaenguita@gmail.com>
2024-06-07 11:36:51 +0200

    refs updated

---
+++ refs/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import StrictMode from './components/ref2/StrictMode'
+import FocusableInput from './components/ref1/FocusableInput'
+import Counter from './components/ref3/Counter'
+
+function App() {
+
+  return (
+    <>
+    <FocusableInput/>
+    <StrictMode/>
+    <Counter/>
+    </>
+  )
+}
+
+export default App
