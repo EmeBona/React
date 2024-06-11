567521e01a09a851dfb94997f27665930e759e58
María Bona <mariabonaenguita@gmail.com>
2024-06-11 11:16:38 +0200

    stylingComponents uploaded

---
+++ stylingComponents/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Welcome from './components/Welcome'
+import './components/index.css'
+
+function App() {
+
+  return (
+    <div className='welcome'>
+      <Welcome name="María"/>
+    </div>
+  )
+}
+
+export default App
